8dd930ee1a4100290d022d66c4739764de1c696f
tacara1234 <alejandro.sumarraga@gmail.com>
2015-10-19 18:48:06 -0500

    quite una variables y la funcion onDeviceReady() porque se repetia en compass.js ahi estan ahora

--- www/js/camera.js
+++ www/js/camera.js
@@ -4,49 +4,31 @@
 
 
 function captureImageSuccess(mediaFiles) {
-    // var i, len;
-    // var formatSuccess = function (mediaFile) {
-    //     document.getElementById('format-data').innerHTML =
-    //             "Height: <strong>" + mediaFile.height + "</strong><br/>" +
-    //             "Width: <strong>" + mediaFile.width + "</strong><br/>";
-    // };
-    // for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-    //     document.getElementById('capture-result').innerHTML = "<strong>" + (i + 1) + "file(s) </strong>";
-    //    mediaFiles[i].getFormatData(formatSuccess, formatError);
-    // }
+    var smallImage = document.getElementById("principal-img");
+    smallImage.src = "data:image/jpeg;base64," + mediaFiles;
 }
 
 function captureImage() {
-    //document.getElementById('format-data').innerHTML = "";
-    //document.getElementById('capture-result').innerHTML = "";
+    
     navigator.device.capture.captureImage(captureImageSuccess, captureError,
-        {limit: 1});
+            {limit: 1});
 }
 
 function captureError(error) {
     var msg = 'An error occurred during capture: ' + error.code;
     navigator.notification.alert(msg, null, 'Error!');
     document.getElementById('capture-result').innerHTML =
-        "<strong>Error</strong>";
+            "<strong>Error</strong>";
 }
 function formatError(error) {
     alert("Error getting file format data: " + error.code);
 }
 
-var pictureSource;
-var destinarionType;
-
-document.addEventListener("deviceready", onDeviceReady, false);
-
-function onDeviceReady() {
-    pictureSource = navigator.camera.PictureSourceType;
-    destinationType = navigator.camera.DestinationType;
-}
-
 function onPhotoDataSuccess(imageData) {
-    var smallImage = document.getElementById("img0");
-    smallImage.style.display = 'block';
-    smallImage.src = "data:image/jpeg;base63," + imageData;
+    var smallImage = document.getElementById("principal-img");
+    //smallImage.style.display = 'block';
+    //smallImage.src = "data:image/jpeg;base63," + imageData;
+    smallImage.src = imageData;
 }
 function onFail(message) {
     alert("Fallo al inicializar la camara " + message);
